3790459949e55de4e704f641643f1fc4b903e382
marthaurion <marthaurion@gmail.com>
2013-11-13 09:48:18 -0600

    basic card data structure is mostly done

--- cards.py
+++ cards.py
@@ -1,28 +1,42 @@
 import random
 
-suits = ['C', 'D', 'H', 'S']
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
 class Card:
-	def __init__(self, value, suit):
-		self.rank = ranks[value]
-		self.value = value
+	suitNames = ['C', 'D', 'H', 'S']
+	rankNames = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+	
+	def __init__(self, rank, suit):
+		self.rank = rank
 		self.suit = suit
 	
 	def __str__(self):
-		return self.rank + self.suit
+		return Card.rankNames[self.rank] + Card.suitNames[self.suit]
+	
+	def __gt__(self, c2):
+		if self.rank == c2.rank:
+			return self.suit > c2.suit
+		else:
+			return self.rank > c2.rank
+	
+	def __lt__(self, c2):
+		if self.rank == c2.rank:
+			return self.suit < c2.suit
+		else:
+			return self.rank < c2.rank
+	
+	def __eq__(self, c2):
+		return self.rank == c2.rank and self.suit == c2.suit
 
 class Deck:
 	def __init__(self):
 		self.cards = []
-		for suit in suits:
-			for rank in ranks:
-				self.cards.append(Card(rank, suit))
+		for i in range(0, 4):
+			for j in range(0, 13):
+				self.cards.append(Card(j, i))
 		random.shuffle(self.cards)
 	
-	def cprint(self):
-		for card in self.cards:
-			print card.rank+card.suit
+	def __str__(self):
+		middle = ", ".join([str(card) for card in self.cards])
+		return "[" + middle + "]"
 
 	def shuffle(self):
 		random.shuffe(self.cards)
@@ -32,14 +46,41 @@ class Deck:
 	
 	def add(self, card):
 		self.cards.append(card)
-
+		
 	def size(self):
 		return len(self.cards)
 
-		
+class Player:
+	def __init__(self):
+		self.cards = []
+	
+	def __str__(self):
+		middle = ", ".join([str(card) for card in self.cards])
+		return "[" + middle + "]"
+	
+	def size(self):
+		return len(self.cards)
+	
+	def sort(self):
+		self.cards.sort()
+	
+	def draw(self, deck, n):
+		for i in range(0,n):
+			self.cards.append(deck.draw())
+		self.sort()
+	
+	# takes an index as input
+	def discard(self, card):
+		return self.cards.pop(card)
+
+	def calcValue(self):
+		pass
 
 d = Deck()
-d.cprint()
+print d
 print d.size()
-print d.draw()
+p1 = Player()
+p1.draw(d, 5)
+print d
 print d.size()
+print p1
